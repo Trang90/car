4cbba68b3a25fa3cae9cbe90ef1db48e2460f015
Trang90 <65718423+Trang90@users.noreply.github.com>
2020-12-17 17:31:11 +0100

    static web

---
+++ js/defaul.js
@@ -0,0 +1,14 @@
+	
+function menumobile()
+	{
+		if($(".navi-middle").css('display')=='none')
+		{
+			$(".navi-middle").fadeIn("fast");
+		}
+		else
+		{
+			$(".navi-middle").fadeOut("fast");
+		}
+	}
+	
+	
